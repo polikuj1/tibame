cdaeb1f1938f7397c0bda1b406bb29743ecf3839
polikuj1 <polikuj1@yahoo.com.tw>
2023-05-17 23:06:17 +0800

    cd旋轉

--- js/playlist.js
+++ js/playlist.js
@@ -3,12 +3,26 @@ console.log(cd);
 cd.forEach(item => {
   item.addEventListener('click', cdRotate);
 })
-
+let angle = 0;
+let isRotate = 0;
+let interval;
 function cdRotate(e) {
-  const cd = e;
-  console.log(cd);
-  console.log(134);
-  // cd.forEach(item => {
-  //   if
-  // })
+  const id = e.target.dataset.cd;
+  cd.forEach(item => {
+    if(item.dataset.cd === id) {
+      if(isRotate === 0) {
+        console.log('判斷是否正在旋轉');
+        isRotate = 1;
+        interval = setInterval(function() {
+            angle += 5;
+            if(angle > 360) {angle = 0};
+            item.style.transform = `rotate(${angle}deg)`;
+          },50);
+      } else {
+        clearInterval(interval);
+        isRotate = 0;
+        console.log('解除定時');
+      }
+    }
+  })
 }
