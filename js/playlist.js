4b5369c553586448978801d2c972a183a2f920c3
張嘉哲 <polikuj1@yahoo.com.tw>
2023-05-21 14:29:04 +0800

    cd功能修改完畢

--- js/playlist.js
+++ js/playlist.js
@@ -2,29 +2,31 @@ const cd = document.querySelectorAll('.cd .pic img');
 const cdTitle = document.querySelectorAll('.cd .title');
 const progress_container = document.querySelector('.progress_container');
 const progress_current = document.querySelector('.current_time');
-// console.log(cdTitle);
 cd.forEach(item => {
   item.addEventListener('click', cdRotate);
 })
 let angle = [0,0,0,0,0];
 let isRotate = 0;
 let interval;
-
 let isPlay = 0;
 let cdOrder;
 function cdRotate(e) {
   const id = e.target.dataset.cd;
   cd.forEach((item,index) => {
     if(item.dataset.cd === id) {
+      if (cdOrder !== index) {
+        stopMusic(cdOrder); // 停止當前正在播放的 CD
+        clearInterval(interval);
+        isRotate = 0;
+      }
       cdOrder = index;
-      console.log(cdOrder);
+      // console.log(cdOrder);
       isPlay = 1;
-      
       txtHover(index);
       progress_container.style.display = 'block';
       if(isRotate === 0) {
         playMusic(index);
-        console.log('判斷是否正在旋轉');
+        // console.log('判斷是否正在旋轉');
         isRotate = 1;
         interval = setInterval(function() {
           angle[index] += 0.5;
@@ -35,7 +37,7 @@ function cdRotate(e) {
         // console.log(angle);
         clearInterval(interval);
         isRotate = 0;
-        console.log('解除定時');
+        // console.log('解除定時');
         isPlay = 0;
         playMusic(index);
       }
@@ -44,7 +46,6 @@ function cdRotate(e) {
     }
   })
 }
-
 
 
 const src = ['Vaundy', '愛', 'Ado', 'milet', 'YOASOBI'];
@@ -65,8 +66,6 @@ const audio = [
     audio: document.createElement('audio'),
   },
 ];
-
-
 let perProgress;
 let playLength = [0,0,0,0,0];
 let totalDuration;
@@ -74,29 +73,21 @@ audio.forEach((item,index) => {
   item.audio.src = `../playlist/${src[index]}.mp3`;
   item.audio.addEventListener('ended', () => {
     clearInterval(interval);
-    clearInterval(timer);
     playLength[cdOrder] = 0;
     isRotate = 0;
-    console.log('解除定時');
+    // console.log('解除定時');
     isPlay = 0;
   })
   item.audio.addEventListener('playing', () => {
     if(cdOrder === index) {
-      console.log(item.audio.duration,'總秒數');
+      // console.log(item.audio.duration,'總秒數');
       totalDuration = item.audio.duration;
       perProgress = (100 / totalDuration);
-      console.log(perProgress,'每秒要跑得趴數');
-      // console.log(item.audio.currentTime, '播放到幾秒');
-      loadingBar(true);
+      // console.log(perProgress,'每秒要跑得趴數');
     }
-    // setInterval(() => {
-    //   playLength += perProgress;
-    //   progress_current.style.width = `${playLength}%`;
-    // }, 1000);
   })
   item.audio.addEventListener('pause', () => {
     console.log(item.audio.currentTime);
-    loadingBar(false);
   })
   item.audio.addEventListener('timeupdate', () => {
     if (cdOrder === index) {
@@ -121,8 +112,7 @@ function playMusic(index) {
     } else {
       item.audio.pause();
     }
-  })
-  
+  }) 
 }
 
 function txtHover(index) {
@@ -135,22 +125,6 @@ function txtHover(index) {
   })
 }
 
-let timer;
-function loadingBar(playOrPause) {
-  if(playOrPause) {
-    clearInterval(timer);
-    console.log(playLength[cdOrder]);
-    timer = setInterval(() => {
-      playLength[cdOrder] += perProgress;
-      progress_current.style.width = `${playLength[cdOrder]}%`;
-    }, 1000);
-    console.log('進度條加載');
-  } else {
-    clearInterval(timer);
-    console.log('暫停進度條');
-  }
-}
-
 progress_container.addEventListener('click', setProgress);
 function setProgress(e) {
   const width = this.clientWidth; 
@@ -161,4 +135,13 @@ function setProgress(e) {
   playLength[cdOrder] = `${clickX / width * 100}`;
   progress_current.style.width = `${playLength[cdOrder]}%`;
   // console.log(playLength[cdOrder]);
+}
+
+
+function stopMusic(index) {
+  if (index !== undefined && index >= 0 && index < audio.length) {
+    console.log('停止播放');
+    audio[index].audio.pause();
+    // audio[index].audio.currentTime = 0;
+  }
 }
